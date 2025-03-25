aef5d256f6bf7e1d541aad5e44c613997570f93e
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2025-03-25 14:54:00 +0000

    atividade coleção

---
+++ atividade coleções/AtvColecoes.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class AtvColecoes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> nomes = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Digite o nome " + (i + 1) + ": ");
+            nomes.add(scanner.nextLine());
+        }
+        Collections.sort(nomes);
+        System.out.println("\nNomes ordenados em ordem alfabética:");
+
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+
+        scanner.close();
+    }
+}
