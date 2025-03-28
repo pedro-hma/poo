8fad27e3b73702a9c9c8aa58fdba028db70dfe34
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2025-03-28 12:52:35 +0000

    Lista 01

--- TesteListas.java
+++ TesteListas.java
@@ -1,12 +1,23 @@
 import java.util.*;
-public class TesteListas{
+
+public class TesteListas {
     public static void main(String[] args) {
-        List <String> l1 = new ArrayList<String>();
-        List <Integer> l2 = new LinkedList<Integer>();
-    }
-    for (int i = 0; i < 5; i++){
-        l1.add ("a"+i);
-        l2.add(i, new Integer(i));
-        // completar
+        List<String> l1 = new ArrayList<>();
+        List<Integer> l2 = new LinkedList<>();
+
+        for (int i = 0; i < 5; i++) {
+            l1.add("a" + i);
+            l2.add(i);
+        }
+
+        for (int j = 0; j < l1.size(); j++) {
+            System.out.println(l1.get(j));
+        }
+
+        for (Integer obj : l2) {
+            System.out.println(obj);
+        }
+
+        System.out.println(l2.contains(5));
     }
 }
